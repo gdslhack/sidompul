c70f3a7716c04be06834b39314dbf8cde0edae7c
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-08-14 22:13:02 +0700

    Update verify-otp.js

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -12,8 +12,26 @@ export default async function handler(req, res) {
   }
 
   try {
+    // Request OTP
+    const otpResponse = await fetch(`https://srg-txl-login-controller-service.ext.dp.xl.co.id/v2/auth/email/${email}/`, {
+      method: 'POST',
+      headers: {
+        'x-dynatrace': 'MT_3_2_763403741_15-0_a5734da2-0ecb-4c8d-8d21-b008aeec4733_30_456_73',
+        'accept': 'application/json',
+        'authorization': 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0',
+        'language': 'en',
+        'version': '4.1.2',
+        'user-agent': 'okhttp/3.12.1',
+      },
+    });
+
+    const otpData = await otpResponse.json();
+    if (otpData.statusCode !== 200) {
+      throw new Error(otpData.statusDescription);
+    }
+
     // Verify OTP
-    const response = await fetch(`https://srg-txl-login-controller-service.ext.dp.xl.co.id/v4/auth/email/${email}/${otp}/000000000000000`, {
+    const loginResponse = await fetch(`https://srg-txl-login-controller-service.ext.dp.xl.co.id/v2/auth/email/${email}/OTP/${otp}/354630570072013?force=true`, {
       method: 'GET',
       headers: {
         'x-dynatrace': 'MT_3_2_763403741_15-0_a5734da2-0ecb-4c8d-8d21-b008aeec4733_30_456_73',
@@ -25,13 +43,12 @@ export default async function handler(req, res) {
       },
     });
 
-    const data = await response.json();
-
-    if (data.statusCode !== 200) {
-      throw new Error(data.statusDescription);
+    const loginData = await loginResponse.json();
+    if (loginData.statusCode !== 200) {
+      throw new Error(loginData.statusDescription);
     }
 
-    const accessToken = data.result.data.accessToken;
+    const accessToken = loginData.result.data.accessToken;
     res.status(200).json({ accessToken });
   } catch (error) {
     res.status(500).json({ error: error.message });
