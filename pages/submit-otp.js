3e5bb82f13d4b2eec0906746b8fbc24f2800290c
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-08-14 23:18:39 +0700

    Update submit-otp.js

--- pages/submit-otp.js
+++ pages/submit-otp.js
@@ -1,19 +1,36 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { login } from '../utils/api';
+import axios from 'axios';
 
 export default function SubmitOtp() {
   const [otp, setOtp] = useState('');
   const router = useRouter();
+  const { email } = router.query;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { email } = router.query;
-    const response = await login(email, otp);
-    if (response.statusCode === 200) {
-      router.push('/check-quotas');
-    } else {
-      alert('Failed to submit OTP');
+    try {
+      const response = await axios.get(
+        `https://srg-txl-login-controller-service.ext.dp.xl.co.id/v2/auth/email/${email}/${otp}/354630570072013?force=true`,
+        {
+          headers: {
+            Accept: 'application/json',
+            authorization: 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0',
+            version: '6.1.1',
+            'user-agent': 'okhttp/4.9.3',
+            Host: 'srg-txl-login-controller-service.ext.dp.xl.co.id'
+          }
+        }
+      );
+
+      if (response.data.statusCode === 200) {
+        router.push('/check-quotas');
+      } else {
+        alert('Failed to submit OTP');
+      }
+    } catch (error) {
+      alert('An error occurred while submitting OTP');
+      console.error(error);
     }
   };
 
