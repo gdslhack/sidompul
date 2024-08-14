886c2657e8eb2b2ffe0c725350c54211502c6dba
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-08-14 23:21:02 +0700

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,34 +1,44 @@
 import axios from 'axios';
 
-const apiBaseUrl = 'https://srg-txl-login-controller-service.ext.dp.xl.co.id/v2/auth';
-
+// Endpoint untuk request OTP
 export const requestOtp = async (email) => {
-  const response = await axios.post(`${apiBaseUrl}/email/${email}/`, null, {
-    headers: {
-      Accept: 'application/json',
-      authorization: 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0',
-      version: '6.1.1',
-      'user-agent': 'okhttp/4.9.3',
-      Host: 'srg-txl-login-controller-service.ext.dp.xl.co.id',
-    },
-  });
+  const response = await axios.post(
+    `https://srg-txl-login-controller-service.ext.dp.xl.co.id/v2/auth/email/${email}/`,
+    null,
+    {
+      headers: {
+        Accept: 'application/json',
+        authorization: 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0',
+        version: '6.1.1',
+        'user-agent': 'okhttp/4.9.3',
+        Host: 'srg-txl-login-controller-service.ext.dp.xl.co.id'
+      }
+    }
+  );
   return response.data;
 };
 
+// Endpoint untuk login dengan OTP
 export const login = async (email, otp) => {
-  const response = await axios.get(`${apiBaseUrl}/email/${email}/${otp}/354630570072013?force=true`, {
-    headers: {
-      Accept: 'application/json',
-      authorization: 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0',
-      version: '6.1.1',
-      'user-agent': 'okhttp/4.9.3',
-      Host: 'srg-txl-login-controller-service.ext.dp.xl.co.id',
-    },
-  });
+  const response = await axios.get(
+    `https://srg-txl-login-controller-service.ext.dp.xl.co.id/v2/auth/email/${email}/${otp}/354630570072013?force=true`,
+    {
+      headers: {
+        Accept: 'application/json',
+        authorization: 'Basic ZGVtb2NsaWVudDpkZW1vY2xpZW50c2VjcmV0',
+        version: '6.1.1',
+        'user-agent': 'okhttp/4.9.3',
+        Host: 'srg-txl-login-controller-service.ext.dp.xl.co.id'
+      }
+    }
+  );
   return response.data;
 };
 
+// Endpoint untuk cek kuota
 export const checkQuota = async (phoneNumber) => {
-  const response = await axios.get(`https://srg-txl-utility-service.ext.dp.xl.co.id/v4/package/check/${phoneNumber}`);
+  const response = await axios.get(
+    `https://srg-txl-utility-service.ext.dp.xl.co.id/v4/package/check/${phoneNumber}`
+  );
   return response.data;
 };
